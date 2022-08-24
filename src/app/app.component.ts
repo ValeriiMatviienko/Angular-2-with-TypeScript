a8d20614f7c7da6511499e9675f285b34f5fcc07
Valerii Matviienko <valerii.matviienko@capgemini.com>
2022-08-24 14:48:59 +0200

    add task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { getTestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular2';
+  rootName = 'Val';
+
+  onNameChanged(newName) {
+    this.rootName = newName;
+  }
 }
