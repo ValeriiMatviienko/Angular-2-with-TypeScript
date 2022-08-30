7c04368638d400e6701c343beebe53cb4fab3732
Valerii Matviienko <valerii.matviienko@capgemini.com>
2022-08-30 08:50:57 +0200

    added task 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard.component';
 import { UserComponent } from './user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, DashboardComponent, UserDetailComponent],
+  declarations: [AppComponent, UserComponent, UserDetailComponent, CartComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
